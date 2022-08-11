99e0505c658815f67d42dbc4d142f7a385eb8db8
Laurence Chen <laurence@replware.dev>
2022-08-11 22:01:29 +0800

    prepare the keys table

--- bin/load.sh
+++ bin/load.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash                                                                                                     
-                                                                                                                        
+
 psql -d queue -c "DROP TABLE IF EXISTS jobs;"                                                                  
+psql -d queue -c "DROP TABLE IF EXISTS keys;"
+
+psql -d queue -c "CREATE TABLE keys (
+id BIGINT,
+name TEXT,
+PRIMARY KEY(id)
+);"
+
 psql -d queue -c "CREATE TABLE jobs (                                                  
-id bigint,
-type text,
-key_id bigint,
-input_json text,
-priority smallint,
-done boolean,
-updated timestamp
-);"                                                                                                                     
+id BIGINT,
+type TEXT,
+key_id BIGINT,
+input_json TEXT,
+priority SMALLINT,
+done BOOLEAN,
+updated TIMESTAMP,
+PRIMARY KEY(id),
+CONSTRAINT fk_key_id
+  FOREIGN KEY(key_id)
+    REFERENCES keys(id)
+      ON DELETE SET NULL
+);"
 # priority 2 > 1 > 0
 # The higher the priority the bigger the integer
 
-#psql -d queue -c "\copy jobs FROM './resources/data.csv' HEADER CSV;"        
 psql -d queue -f ./resources/input-data.sql
--- resources/input-data.sql
+++ resources/input-data.sql
@@ -1,4 +1,8 @@
-INSERT INTO jobs VALUES
+INSERT INTO keys (id, name) VALUES
+  (13, 'The key 13')
+, (14, 'The key 14');
+
+INSERT INTO jobs (id, type, key_id, input_json, priority, done, updated) VALUES
   (0,'x',13,'{"a":1}',0,false,NOW())
 , (1,'y',14,'{"b":1}',1,false,NOW()+ '1 hour')
-, (2,'x',13,'{"a":2}',2,false,NOW()+ '2 hours')
+, (2,'x',13,'{"a":2}',2,false,NOW()+ '2 hours');
